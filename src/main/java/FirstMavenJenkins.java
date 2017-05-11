ae0b68b4f2a50938984fc88698588b518f2dcf04
Sai Upadhyay <saupadhyay@vistaprint.com>
2017-05-11 10:39:40 -0400

    First Commit of the project

---
+++ src/main/java/FirstMavenJenkins.java
@@ -0,0 +1,15 @@
+/**
+ * Created by saupadhyay on 5/11/2017.
+ */
+public class FirstMavenJenkins {
+    public static void main(String[] args)
+    {
+        System.out.println(printy());
+    }
+
+    public static String printy()
+    {
+        String txt = "Hello there ...yay!";
+        return txt;
+    }
+}
---
+++ src/test/java/FirstMavenJenkinsTest.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by saupadhyay on 5/11/2017.
+ */
+
+public class FirstMavenJenkinsTest {
+     @Test
+     public void testprinty()
+     {
+          Assert.assertEquals("Hello there ...yay!",FirstMavenJenkins.printy());
+     }
+
+}
